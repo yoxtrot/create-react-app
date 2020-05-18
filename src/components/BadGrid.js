32e06a5510e5fe02efc41d3488facdbb6e128ac4
Forest Yox <yoxtrot@gmail.com>
2020-05-18 15:18:29 -0600

    initial commit

---
+++ src/components/BadGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function BadGrid() {
+    return(
+        <div className="BadGrid">
+            <Button>Foobar</Button>
+
+        </div>
+    )
+}
